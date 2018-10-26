2d83251e82d100024a9f10d8be65435a527fd538
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-10-26 10:09:55 -0700

    run a function and push value into an array several times

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+//  Business Logic
+function arrayOfNumbers(numbers) {
+  var numberArray = [];
+  for(var i = 0; i <= numbers; i++) {
+    numberArray.push(numbers);
+  }
+  return numberArray;
+};
+
+
+
+
+
+
+// UI Logic
 $(document).ready(function() {
   $(".userInput").submit(function(event) {
     event.preventDefault();
     var userName = $("#userName").val();
     var inputNumber = parseInt($("#inputNumber").val());
 
-
-    $("#translatedNumbers").append(inputNumber);
+    var resultArray = arrayOfNumbers(inputNumber);
+    console.log(inputNumber, resultArray);
+    $("#translatedNumbers").append(resultArray);
   });
 });
