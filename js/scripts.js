29f083334b87ab3c767b048ae2a3e2484083d73a
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-10-26 16:45:25 -0700

    delete any function for setTimeout

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function arrayOfNumbers(numbers, name) {
   var numberArray = [];
   for(var i = 0; i <= numbers; i++) {
     if (i % 3 === 0 && i != 0) {
-      numberArray.push("Im Sorry " + name + ", I'm afraid I cannot do that");
+      numberArray.push("I'm Sorry " + name + ", I'm afraid I cannot do that");
     } else if (i === 0 || i.toString().includes("0")) {
       numberArray.push("Beep");
     } else if (i === 1 || i.toString().includes("1")) {
@@ -11,7 +11,8 @@ function arrayOfNumbers(numbers, name) {
     } else {
       numberArray.push(i);
     }
-  }; 2000;
+
+  }
   return numberArray.join("! ");
 };
 
@@ -34,12 +35,8 @@ $(document).ready(function() {
     alert("Please make sure input number is an integer >= 0")
   }
 
-  var splitArray = resultArray.split("");
-  // for (var j = 0; j <= splitArray.length; ++j){
-  //   $("#translatedNumbers").append(j);
-  // }; 2000;
-  // console.log(j);
-  $("#translatedNumbers").text(resultArray);
+  $("p").text(resultArray);
+
 
   });
 });
