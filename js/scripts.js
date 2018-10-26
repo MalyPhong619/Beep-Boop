4407aed016cd291f3af40277e783ef56bcfa2fed
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-10-26 11:30:59 -0700

    moves around if statement conditions

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@
 function arrayOfNumbers(numbers, name) {
   var numberArray = [];
   for(var i = 0; i <= numbers; i++) {
-    if (i === 0 || i.toString().includes("0")) {
+    if (i % 3 === 0 && i != 0) {
+      numberArray.push("Im Sorry " + name + ", I'm afraid I cannot do that!");
+    } else if (i === 0 || i.toString().includes("0")) {
       numberArray.push("Beep");
     } else if (i === 1 || i.toString().includes("1")) {
       numberArray.push("Boop");
-    } else if (i % 3 === 0) {
-      numberArray.push("Im Sorry " + name + ", I'm afraid I cannot do that!")
     } else {
       numberArray.push(i);
     }
