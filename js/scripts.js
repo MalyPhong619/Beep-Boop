bd98533b5892c18e51a02a56859bdc901a820264
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-10-26 10:55:17 -0700

    run function when fields are filled & .includes doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 function arrayOfNumbers(numbers, name) {
   var numberArray = [];
   for(var i = 0; i <= numbers; i++) {
-    if (i === 0) {
-    numberArray.push("Beep!");
-  } else if (i === 1) {
-    numberArray.push("Boop!");
-  } else if (i % 3 === 0) {
-    numberArray.push("Im Sorry " + name + ", I'm afraid I cannot do that!")
-  }
+    if (i === 0 || i.includes(0) === 0) {
+      numberArray.push("Beep!");
+    } else if (i === 1) {
+      numberArray.push("Boop!");
+    } else if (i % 3 === 0) {
+      numberArray.push("Im Sorry " + name + ", I'm afraid I cannot do that!")
+    }
   }
   return numberArray;
 };
@@ -25,8 +25,13 @@ $(document).ready(function() {
     var userName = $("#userName").val();
     var inputNumber = parseInt($("#inputNumber").val());
 
+    if (inputNumber >= 0 && userName) {
     var resultArray = arrayOfNumbers(inputNumber, userName);
     console.log(inputNumber, resultArray);
+  } else {
+    alert("Please make sure all fields are filled out");
+  }
+
     $("#translatedNumbers").append(resultArray);
   });
 });
